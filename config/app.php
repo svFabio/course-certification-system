00670fda5f7b3186d2a9965167889962d0c3f68f
svFabio <svfabio.andres@gmail.com>
2026-09-08 10:00:00 -0400

    chore: initial Laravel project scaffolding
    - Laravel 11 with PSR-4 autoload
    - Tailwind CSS + Vite build pipeline
    - PHPUnit/Pest configuration with SQLite testing
    - Config files: app, cache, database, filament, permission, queue, session, mail, logging, filesystems
    - Storage directory structure with .gitkeep files

---
+++ config/app.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    'name' => env('APP_NAME', 'Plataforma de Formación Continua'),
+    'env' => env('APP_ENV', 'production'),
+    'debug' => (bool) env('APP_DEBUG', false),
+    'url' => env('APP_URL', 'http://localhost'),
+    'timezone' => env('APP_TIMEZONE', 'America/La_Paz'),
+    'locale' => env('APP_LOCALE', 'es'),
+    'fallback_locale' => env('APP_FALLBACK_LOCALE', 'en'),
+    'faker_locale' => env('APP_FAKER_LOCALE', 'es_ES'),
+    'cipher' => 'AES-256-CBC',
+    'key' => env('APP_KEY'),
+    'previous_keys' => [
+        ...array_filter(
+            explode(',', env('APP_PREVIOUS_KEYS', ''))
+        ),
+    ],
+    'maintenance' => [
+        'driver' => env('APP_MAINTENANCE_DRIVER', 'file'),
+        'store' => env('APP_MAINTENANCE_STORE', 'database'),
+    ],
+
+];
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './resources/js/**/*.vue',
+        './resources/js/**/*.js',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Inter', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                umss: {
+                    blue: '#003087',
+                    green: '#007A3D',
+                },
+            },
+        },
+    },
+
+    plugins: [],
+};
